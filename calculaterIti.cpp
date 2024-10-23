5830a77222f674ed1113f43533deb11602f30816
MernaTarek55 <91115322+MernaTarek55@users.noreply.github.com>
2024-10-23 16:35:04 +0300

    reset comit

--- calculaterIti.cpp
+++ calculaterIti.cpp
@@ -1,18 +1,7 @@
+// calculaterIti.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
-#include <string>
-using namespace std;
-
-int Multiplication(int number1, int number2) {
-    return number1 * number2;
-}
-
-int power(int number1, int number2) {
-    return number1^number2;
-}
-
-int Addition (int number1, int number2) {
-return number1 + number2;
-}
 
 float Division (float number1, float number2) {
     return number1 / number2;
@@ -37,68 +26,10 @@ double SquareRoot(double number)
     return s;
 }
 
-
-int extractNumber(const string& operation, int& index) {
-    string numberStr = "";
-
-    while (index < operation.size() && operation[index] == ' ')
-        index++;
-
-    while (index < operation.size() && isdigit(operation[index])) {
-        numberStr += operation[index];
-        index++;
-    }
-
-    return stoi(numberStr);
+int main()
+{
+    std::cout << "Hello World!\n";
 }
-
-int main() {
-    string operation;
-    cout << "Welcome to our Calculator, please enter the operation: ";
-    getline(cin, operation);
-
-    int index = 0;
-
-    int number1 = extractNumber(operation, index);
-
-    while (index < operation.size() && operation[index] == ' ')
-        index++;
-
-    char op = operation[index];
-    index++;
-
-    while (index < operation.size() && operation[index] == ' ')
-        index++;
-
-    int number2 = extractNumber(operation, index);
-
-    int result;
-    switch (op) {
-    case '+':
-        result = Addition(number1, number2);
-        break;
-    case '/':
-        result = Division(number1, number2);
-        break;
-    case '!':
-        result = Factorial(number1);
-        break;
-    case '*':
-        result = Multiplication(number1, number2);
-        break;
-    case '^':
-        result = power(number1, number2);
-        break;
-    default:
-        cout << "Invalid operator!" << endl;
-        return 1;
-    }
-
-    cout << "Result: " << result << endl;
-
-    return 0;
-}
-
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
